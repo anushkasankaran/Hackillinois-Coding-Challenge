3f8a66c669377397f744f5f5ff848642520c1488
anushkasankaran <55302555+anushkasankaran@users.noreply.github.com>
2023-09-07 10:56:13 -0500

    Initial Commit

---
+++ Hackillinois Coding Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hackillinois Coding Challenge
+//
+//  Created by Anushka Sankaran on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hackillinois Coding Challenge/Hackillinois_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackillinois_Coding_ChallengeApp.swift
+//  Hackillinois Coding Challenge
+//
+//  Created by Anushka Sankaran on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Hackillinois_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
